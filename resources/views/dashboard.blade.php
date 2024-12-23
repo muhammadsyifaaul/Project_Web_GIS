c3e7d0d7661320970b5c5299b7f04c520db25001
RaihanMhrdhka <raihanmhrdhka.com>
2024-12-23 14:35:17 +0700

    test commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard</title>
+    <title>dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
-
+<!-- test -->
 <body>
     <div class="container mt-5">
         <div class="row custom-table mt-5 rounded-3">
