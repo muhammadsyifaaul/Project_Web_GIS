73b82a6b15262e963fe209bba5b2343e0a1f2e7b
RaihanMhrdhka <raihanmhrdhka.com>
2024-12-24 15:07:36 +0700

    update

---
+++ layout/_sidenav.php
@@ -0,0 +1,23 @@
+<div class="main-sidebar sidebar-style-2">
+  <aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+      <a href="index.php">
+        <img src="../assets/img/logo Dis.png" alt="logo" width="150">
+      </a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+      <a href="index.php">EF</a>
+    </div>
+    <ul class="sidebar-menu">
+      <li class="menu-header">Dashboard</li>
+      <li><a class="nav-link" href="../Dashboard/index.php"><i class="fas fa-fire"></i> <span>Home</span></a></li>
+      <li class="menu-header">Main Feature</li>
+      <li class="dropdown">
+        <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Lihat Data</span></a>
+        <ul class="dropdown-menu">
+          <li><a class="nav-link" href="../list/index.php">List</a></li>
+          <li><a class="nav-link" href="../list/create.php">Tambah Data</a></li>
+        </ul>
+    </ul>
+  </aside>
+</div>
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,108 @@
+<?php
+require_once '../layout/_top.php';
+require_once '../helper/connection.php';
+
+$penyakit = mysqli_query($connection, "SELECT COUNT(*) FROM penyakit");
+$gejala = mysqli_query($connection, "SELECT COUNT(*) FROM gejala");
+$obat = mysqli_query($connection, "SELECT COUNT(*) FROM obat");
+
+$penyakit = mysqli_fetch_array($penyakit)[0];
+$gejala = mysqli_fetch_array($gejala)[0];
+$obat = mysqli_fetch_array($obat)[0];
+?>
+
+<section class="section">
+  <div class="section-header">
+    <h1>Dashboard</h1>
+  </div>
+  
+  <!-- Kartu Statistik -->
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+      <div class="card card-statistic-1">
+        <div class="card-icon bg-success">
+          <i class="fas fa-seedling"></i> <!-- Ikon Tanaman -->
+        </div>
+        <div class="card-wrap">
+          <div class="card-header">
+            <h4>Total Lokasi</h4>
+          </div>
+          <div class="card-body">
+            <?= $penyakit ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+      <div class="card card-statistic-1">
+        <div class="card-icon bg-warning">
+          <i class="fas fa-diagnoses"></i> <!-- Ikon Gejala -->
+        </div>
+        <div class="card-wrap">
+          <div class="card-header">
+            <h4>Total No Telepon</h4>
+          </div>
+          <div class="card-body">
+            <?= $gejala ?>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+      <div class="card card-statistic-1">
+        <div class="card-icon bg-info">
+          <i class="fas fa-prescription-bottle-alt"></i> <!-- Ikon Obat -->
+        </div>
+        <div class="card-wrap">
+          <div class="card-header">
+            <h4>Total Foto</h4>
+          </div>
+          <div class="card-body">
+            <?= $obat ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <!-- Tabel Ringkasan -->
+  <div class="row mt-4">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h4>Ringkasan Data</h4>
+        </div>
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Kategori</th>
+                <th>Jumlah</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Total Kecamatan</td>
+                <td><?= $penyakit ?></td>
+              </tr>
+              <tr>
+                <td>Total Kelurahan</td>
+                <td><?= $gejala ?></td>
+              </tr>
+              <tr>
+                <td>Total Gambar</td>
+                <td><?= $obat ?></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+require_once '../layout/_bottom.php';
+?>
