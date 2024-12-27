c65c1597b0c32eaab418333768e0824edb9383e1
ArdanaYazidDirrisqo <ardanasugiarto@gmail.com>
2024-12-27 13:43:21 +0700

    add page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="login-page">
+<div class="login-container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -70,4 +80,8 @@
         </div>
     </div>
 </div>
+</div>
 @endsection
+
+</body>
+</html>
